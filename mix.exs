89a26320b33615259c75731a07514d0d9899cc58
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-12-03 09:25:45 +0700

    Forgot to add phoenix and plug to applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule PhoenixTokenPlug.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:phoenix, :plug, :logger]]
   end
 
   # Dependencies can be Hex packages:
