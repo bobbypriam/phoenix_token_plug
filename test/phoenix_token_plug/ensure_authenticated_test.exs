378cca8809bf2d01ff5d5e80bd3d4681caf91369
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-12-03 09:58:08 +0700

    Add tests for EnsureAuthenticated

---
+++ test/phoenix_token_plug/ensure_authenticated_test.exs
@@ -0,0 +1,32 @@
+defmodule Phoenix.Token.Plug.EnsureAuthenticatedTest do
+  use ExUnit.Case, async: true
+
+  import Plug.Conn
+
+  alias Phoenix.Token.Plug.EnsureAuthenticated
+
+  defmodule UnauthenticatedHandler do
+    def unauthenticated(conn, _params) do
+      conn |> put_status(401) |> assign(:unauthenticated, true)
+    end
+  end
+
+  @opts handler: UnauthenticatedHandler
+  @user %{id: 1}
+
+  test "does nothing if conn.assigns.user exists" do
+    conn = conn() |> assign(:user, @user) |> EnsureAuthenticated.call(@opts)
+    assert conn.status != 401
+  end
+
+  test "calls unauthenticated handler if conn.assigns.user does not exist" do
+    conn = conn() |> EnsureAuthenticated.call(@opts)
+    assert conn.status == 401
+    assert conn.assigns.unauthenticated
+  end
+
+  defp conn do
+    %Plug.Conn{}
+  end
+
+end
